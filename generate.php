386e564fce965592b40b70a97f8dfe4c3deebcf8
Alisher Tog'ayev <38793933+Alisherka7@users.noreply.github.com>
2018-12-25 03:38:46 -0800

    Add files via upload

---
+++ generate.php
@@ -0,0 +1,56 @@
+<?php
+
+require "vendor/autoload.php";
+
+if (!$_GET['text']) {
+    header("location: index.php");
+    die();
+}
+
+$Bar = new Picqer\Barcode\BarcodeGeneratorHTML();
+$code = $Bar->getBarcode($_GET['text'], $Bar::TYPE_CODE_128);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Generate Bar Codes</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .bg {
+            background-image: url("images/bg.jpg");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        #qrbox>div {
+            margin: auto;
+        }
+    </style>
+</head>
+<body class="bg">
+    <div class="container" id="panel">
+        <br><br><br>
+        <div class="row">
+            <div class="col-md-6 offset-md-3" style="background: white; padding: 20px; box-shadow: 10px 10px 5px #888888;">
+                <div class="panel-heading">
+                    <h1>Generate Bar Codes in PHP</h1>
+                </div>
+                <hr>
+                <div id="qrbox">
+                    <?php echo $code ?>
+                </div>
+                <hr>
+                <a href="index.php">Generate Again</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Generate Bar Codes</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .bg {
+            background-image: url("images/bg.jpg");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        #qrbox>div {
+            margin: auto;
+        }
+    </style>
+</head>
+<body class="bg">
+    <div class="container" id="panel">
+        <br><br><br>
+        <div class="row">
+            <div class="col-md-6 offset-md-3" style="background: white; padding: 20px; box-shadow: 10px 10px 5px #888888;">
+                <div class="panel-heading">
+                    <h1>Generate Bar Codes in PHP</h1>
+                </div>
+                <hr>
+                <form action="generate.php" method="get">
+                    <input type="text" style="border-radius: 0px;" name="text" class="form-control" autocomplete="off" placeholder="Text..." value="">
+                    <br>
+                    <button type="submit" class="btn btn-block btn-md btn-outline-success">Generate</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
